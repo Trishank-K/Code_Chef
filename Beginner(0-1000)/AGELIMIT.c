8ccfa66d8864d9317c49168db287d7da053b5e08
Trishank <itstrishank@gmail.com>
2023-04-24 10:46:38 +0530

    Initial Commit

---
+++ Beginner(0-1000)/AGELIMIT.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+	int ntest,min,curr,max;
+	char Out[3];
+	scanf("%d",&ntest);
+	for(int i=1;i<=ntest;i++)
+	{
+	    scanf("%d %d %d",&min,&max,&curr);
+	    curr >= min ? (curr < max ? printf("YES") : printf("NO")) : printf("NO");
+	    // printf("%s\n",Out);
+	}
+	return 0;
+}
+
